8105025fdeadb6ce6e960e4087254b75c7811514
dualistmage <dualistmage@nate.com>
2010-01-12 18:01:58 +0900

    Update thread example of java

---
+++ java/thread_test/test.java
@@ -0,0 +1,35 @@
+public class test
+{
+    public static void main(String[] args) throws Exception {
+        MyRunnable t1 = new MyRunnable(1);
+        MyRunnable t2 = new MyRunnable(2);
+        Thread a = new Thread(t1);
+        Thread b = new Thread(t2);
+        a.start();
+        b.start();
+    } // end - main()
+} // end - test
+
+class MyRunnable implements Runnable {
+
+    private int sleepTime_;
+
+    public MyRunnable(int sleepTime) {
+        sleepTime_ = sleepTime;
+    } // end - MyRunnable
+
+    public void run() {
+        while(true) {
+            // ---------------------------------------------
+            // This is the area which thread is going to do.
+            // ---------------------------------------------
+            try {
+                Thread.sleep( sleepTime_ * 1000 );
+            } catch ( InterruptedException e ) {
+            }
+            System.out.println("Waiting " + sleepTime_ + " secs");
+        }
+    }
+
+} // end - MyRunnable
+
