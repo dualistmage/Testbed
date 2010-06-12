6d4e66d922eb0480981fa16bb63b2ef957608335
Dohyun Yun <dualistmage@wisenut.co.kr>
2010-06-12 23:26:17 +0900

    Added bash example dir

---
+++ bash/exit/exit.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "Test"
+echo "Exit Status : " $?    # Exit Status is 0
+
+slkerjlwekrjek              # Unrecognized command
+echo "Exit Status : " $?    # Exit Status is Non-zero
